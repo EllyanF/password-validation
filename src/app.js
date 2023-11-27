8ce08c47512642dc3aab5a80af5eebce99c70486
Ellyan Fernandes <ellyan.fernandes@hotmail.com>
2023-11-27 17:59:17 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const validatePassword = require('./services/passwordValidator');
+const port = process.env.PORT || 3000;
+
+app.post('/validate-password', validatePassword ,(req, res) => {
+
+    return res.status(204).json();
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ src/services/passwordValidator.js
@@ -0,0 +1,36 @@
+const validatePassword = (req, res, next) => {
+    const password = req.query.password;
+
+    if (password.length <8) {
+        return res.status(400).json(
+            {error: 'Password must contain at least 8 characters.'}
+        );
+    }
+
+    if (!password.match(/^.*[a-z].*$/gm)) {
+        return res.status(400).json({
+            error: 'Password must have at least one lowercase character.'
+        });
+    }
+
+    if (!password.match(/^.*[A-Z].*$/gm)) {
+        return res.status(400).json({
+            error: 'Password must have at least one uppercase character.'
+        });
+    }
+
+    if (!password.match(/^.*[0-9].*$/gm)) {
+        return res.status(400).json({
+            error: 'Password must have at least one number.'
+        });
+    }
+
+    if (!password.match(/^.*[!||@||#||$||&||%||*].*$/gm)) {
+        return res.status(400).json({
+            error: 'Password must have at least one special character.'
+        });
+    }
+
+    next();
+}
+module.exports = validatePassword;
